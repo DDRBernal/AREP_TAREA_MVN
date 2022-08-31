5053dcaf6ac136fd61be8a2d68dfa2b05e5c3f68
David.O <46855679+DDRBernal@users.noreply.github.com>
2022-08-30 23:07:34 -0500

    JSON format added

--- src/main/resources/files/Scripts/connection.js
+++ src/main/resources/files/Scripts/connection.js
@@ -9,23 +9,15 @@ function getData() {
     document.getElementById("Time_Series_Weekly").checked ? time_series = "TIME_SERIES_WEEKLY" : ""
     document.getElementById("Time_Series_Monthly").checked ? time_series = "TIME_SERIES_MONTHLY" : ""
     //https://fast-hollows-06971.herokuapp.com/
+    //console.log('https://www.alphavantage.co/query?function='+time_series+'&symbol='+word_share_traded+'&apikey=demo')
     fetch('https://www.alphavantage.co/query?function='+time_series+'&symbol='+word_share_traded+'&apikey=demo')
         .then(results => results.json()).then(data => vector_result = data).then(data => {
-            const table_elements = document.querySelector('#table_elements')
-            table_elements.innerHTML = ""; delete data ['Meta Data'];
-            for (let i in data) {
-                for (let j in data[i]) {
-                    const row = document.createElement("tr");
-                    const cell = document.createElement("th");
-                    table_elements.appendChild(row);
-                    cell.innerHTML = j;
-                    row.appendChild(cell);
-                    for (let k in data[i][j]) {
-                        const cellText = document.createElement("td");
-                        cellText.innerHTML = data[i][j][k];
-                        row.appendChild(cellText);
-                    }
-                }
-            }
-        }).catch(error => console.log(error))
+        var jsonStr = JSON.stringify(data, null, 4);
+        const table_elements = document.querySelector('#table_elements')
+        delete data ['Meta Data'];
+        table_elements.innerHTML = jsonStr;
+    })
 }
+
+
+
